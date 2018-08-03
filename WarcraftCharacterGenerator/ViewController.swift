7dbff1e3b94776a34d7dd6b97b8a2a9988c954ab
Charlemagne <neoregaf@gmail.com>
2018-08-03 07:28:22 -0400

    Initial Commit

---
+++ WarcraftCharacterGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarcraftCharacterGenerator
+//
+//  Created by Chad Fager on 8/3/18.
+//  Copyright © 2018 Norfare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
